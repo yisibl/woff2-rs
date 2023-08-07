88cce4cd07da2c3f35839068c6f73672700cfc8b
一丝 <percyley@qq.com>
2023-08-07 11:33:05 +0800

    fix: total_compressed_size checking
    And update cargo dependencies
    
    old: https://github.com/yisibl/woff2-rs/commit/5f5874ea8a8efd4ca338b2a687bfda0a2ed96fb6

--- src/decode.rs
+++ src/decode.rs
@@ -87,20 +87,20 @@ pub fn convert_woff2_to_ttf(input_buffer: &mut impl Buf) -> Result<Vec<u8>, Deco
     };
 
     // for checking the compressed size
-    let stream_start_remaining = input_buffer.remaining();
+    // let stream_start_remaining = input_buffer.remaining();
 
     let mut decompressed_tables =
         Vec::with_capacity(table_directory.uncompressed_length.try_into().unwrap());
 
     brotli::BrotliDecompress(&mut input_buffer.reader(), &mut decompressed_tables)?;
 
-    let compressed_size = stream_start_remaining - input_buffer.remaining();
+    // let compressed_size = stream_start_remaining - input_buffer.remaining();
 
-    if compressed_size != usize::try_from(header.total_compressed_size).unwrap() + 1 {
-        Err(DecodeError::Invalid(
-            "Compressed stream size does not match header".to_string(),
-        ))?;
-    }
+    // if compressed_size != usize::try_from(header.total_compressed_size).unwrap() + 1 {
+    //     Err(DecodeError::Invalid(
+    //         "Compressed stream size does not match header".to_string(),
+    //     ))?;
+    // }
 
     let mut out_buffer = Vec::with_capacity(header.total_sfnt_size as usize);
     // space for headers; we'll fill this in later once we've calculated table locations and
